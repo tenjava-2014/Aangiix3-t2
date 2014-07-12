02e569a5a815677ea386a7b58ef1e186f6142a25
Aangiix3 <befour100@yahoo.it>
2014-07-12 12:49:36 +0200

    Events

--- src/main/java/com/tenjava/entries/Aangiix3/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/Aangiix3/t2/TenJava.java
@@ -1,13 +1,31 @@
 package com.tenjava.entries.Aangiix3.t2;
 
+import org.bukkit.Server;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TenJava extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
+		final Server sr = this.getServer();
+		sr.getPluginManager().registerEvents(this, this);
 	}
 	@Override
 	public void onDisable() {
+	}
+	@EventHandler
+	public void onJoin(final PlayerJoinEvent e) {
+	}
+	@EventHandler
+	public void onQuit(final PlayerQuitEvent e) {
+
+	}
+	@EventHandler
+	public void onEntityInteract(final PlayerInteractEntityEvent e) {
+
 	}
 }
