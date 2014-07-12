d8ced1cbfdf4399214c8e9eb28a47f844ea9e464
Aangiix3 <befour100@yahoo.it>
2014-07-12 11:29:17 +0200

    Let's go! - Added basic stuff

--- src/main/java/com/tenjava/entries/Aangiix3/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/Aangiix3/t2/TenJava.java
@@ -1,5 +1,13 @@
 package com.tenjava.entries.Aangiix3.t2;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin implements Listener {
+	@Override
+	public void onEnable() {
+	}
+	@Override
+	public void onDisable() {
+	}
+}
