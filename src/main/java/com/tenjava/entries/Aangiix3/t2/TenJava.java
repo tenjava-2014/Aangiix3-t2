10b44f7cf2822326de961642085e4fa7a13395f8
Aangiix3 <befour100@yahoo.it>
2014-07-12 15:16:23 +0200

    Added Combat Checks

--- src/main/java/com/tenjava/entries/Aangiix3/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/Aangiix3/t2/TenJava.java
@@ -1,25 +1,36 @@
 package com.tenjava.entries.Aangiix3.t2;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.ThrownPotion;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TenJava extends JavaPlugin implements Listener {
 	private MySQL db;
 	private final Map<String, String> requests = new HashMap<String, String>();
+	private final Map<String, Long> timeouts = new HashMap<String, Long>();
+	private final Map<String, Long> tagged = new HashMap<String, Long>();
+	private ItemStack[] armorkit, invkit;
 	private String duelrequest, alreadyrequested, requestsent, requestaccepted, acceptedrequest;
 	private long timeout = 60000L;
+	private boolean ownstuff = false, useincombat = false;
 
 	@Override
 	public void onEnable() {
@@ -30,6 +41,7 @@ public class TenJava extends JavaPlugin implements Listener {
 	}
 	@Override
 	public void onDisable() {
+		tagged.clear();
 		db.kill();
 	}
 	@EventHandler
@@ -38,6 +50,35 @@ public class TenJava extends JavaPlugin implements Listener {
 	@EventHandler
 	public void onQuit(final PlayerQuitEvent e) {
 		requests.remove(e.getPlayer().getName());
+	}
+	/*
+	 * COMBAT CHECKS
+	 */
+	@EventHandler
+	public void onDeath(final PlayerDeathEvent e) { // COMBAT CHECK
+		if (useincombat) return;
+		tagged.remove(e.getEntity().getName());
+	}
+	@EventHandler(ignoreCancelled = true)
+	public void onEntityDamage(final EntityDamageByEntityEvent e) { // COMBAT CHECK
+		if (useincombat) return;
+		final Entity en = e.getEntity();
+		if (en instanceof Player) {
+			final Player p = (Player) en;
+			final Entity en2 = e.getDamager();
+			Player damager = null;
+			if (en2 instanceof Player) {
+				damager = (Player) en2;
+			} else if (en2 instanceof Arrow && ((Arrow)en2).getShooter() instanceof Player) {
+				damager = (Player) ((Arrow)en2).getShooter();
+			} else if (en2 instanceof ThrownPotion && ((ThrownPotion)en2).getShooter() instanceof Player) {
+				damager = (Player) ((ThrownPotion)en2).getShooter();
+			}
+			if (damager == null) return;
+			tagged.put(damager.getName(), System.currentTimeMillis());
+			tagged.put(p.getName(), System.currentTimeMillis());
+		}
+		return;
 	}
 	@EventHandler
 	public void onEntityInteract(final PlayerInteractEntityEvent e) {
@@ -60,6 +101,7 @@ public class TenJava extends JavaPlugin implements Listener {
 				p.sendMessage(requestsent);
 				p2.sendMessage(duelrequest.replaceAll("%ply", pname));
 				requests.put(pname2, pname);
+				timeouts.put(pname2, System.currentTimeMillis() + timeout);
 			}
 			return;
 		}
@@ -71,8 +113,35 @@ public class TenJava extends JavaPlugin implements Listener {
 		alreadyrequested = ChatColor.translateAlternateColorCodes('&', config.getString("messages.alreadyrequested"));
 		requestaccepted = ChatColor.translateAlternateColorCodes('&', config.getString("messages.requestaccepted"));
 		acceptedrequest = ChatColor.translateAlternateColorCodes('&', config.getString("messages.acceptedrequest"));
-		timeout = config.getInt("settings.timeout") * 1000L;
+		timeout = config.getInt("settings.timeout") * 60000L;
+		ownstuff = config.getBoolean("settings.ownstuff");
+		useincombat = config.getBoolean("settings.useincombat");
+		armorkit = new ItemStack[4];
+		short count = 0;
+		for (final String s : Arrays.asList("kit.armor.helmet", "kit.armor.chestplate", "kit.armor.leggings", "kit.armor.boots")) {
+			armorkit[count] = getItemStack(config.getString(s));
+			count++;
+		}
+		count = 0;
+		for (final String s : config.getStringList("kit.inventory")) {
+			invkit[count] = getItemStack(s);
+		}
 		db = new MySQL(config.getString("mysql.url"), config.getString("mysql.username"), config.getString("mysql.password"));
+	}
+	private ItemStack getItemStack(final String s) {
+		short value = 0;
+		final String[] split1 = s.split(" ");
+		String itemID = split1[0];
+		if (split1[0].contains(":")) {
+			final String[] split2 = split1[0].split(":");
+			itemID = split2[0];
+			value = Short.parseShort(split2[1]);
+		}
+		try {
+			return new ItemStack(Integer.parseInt(itemID), Integer.parseInt(split1[1], value));
+		} catch (final Exception ex) {
+			return new ItemStack(Material.AIR);
+		}
 	}
 	private void startGame(final DuelData d, final DuelData d2) {
 
