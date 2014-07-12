b0d9a880191e43c1ed47dc62f1d0f204a65f5ae5
Aangiix3 <befour100@yahoo.it>
2014-07-12 13:11:00 +0200

    Added config and added basic stuff

--- README.md
+++ README.md
@@ -10,8 +10,6 @@ This is a submission for the 2014 ten.java contest.
 - __MC Version:__ 1.7.9 (latest Bukkit beta)
 - __Stream URL:__ https://twitch.tv/Aangiix3
 
-<!-- put chosen theme above -->
-
 ---------------------------------------
 
 Compilation
@@ -30,6 +28,11 @@ Usage
 
 1. Install plugin
 2. Do things with it
+3. ???
+
+Features
+-----
+- Duel with other players
 
 <!-- Hi, Aangiix3! This is the default README for every ten.java submission. -->
 <!-- We encourage you to edit this README with some information about your submission – keep in mind you'll be scored on documentation! -->
--- src/main/java/com/tenjava/entries/Aangiix3/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/Aangiix3/t2/TenJava.java
@@ -1,6 +1,12 @@
 package com.tenjava.entries.Aangiix3.t2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bukkit.Server;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
@@ -9,9 +15,15 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TenJava extends JavaPlugin implements Listener {
+	final Map<String, String> requests = new HashMap<String, String>();
+	private String duelrequest = "%ply has sent you a duel request. Accept it by right clicking him.";
+	private long timeout = 60000L;
+
 	@Override
 	public void onEnable() {
 		final Server sr = this.getServer();
+		saveDefaultConfig();
+		loadConfig();
 		sr.getPluginManager().registerEvents(this, this);
 	}
 	@Override
@@ -22,10 +34,25 @@ public class TenJava extends JavaPlugin implements Listener {
 	}
 	@EventHandler
 	public void onQuit(final PlayerQuitEvent e) {
-
+		requests.remove(e.getPlayer().getName());
 	}
 	@EventHandler
 	public void onEntityInteract(final PlayerInteractEntityEvent e) {
+		final Entity en = e.getRightClicked();
+		if (en instanceof Player) {
+			final Player p = (Player) en;
+			if (requests.containsKey(p.getName())) {
+				// TODO
+			} else {
+				p.sendMessage(duelrequest);
+				requests.put(p.getName(), e.getPlayer().getName());
+			}
 
+		}
+	}
+	private void loadConfig() {
+		final FileConfiguration config = this.getConfig();
+		duelrequest = config.getString("duelrequest");
+		timeout = config.getInt("timeout") * 1000L;
 	}
 }
